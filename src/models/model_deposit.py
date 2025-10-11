3ee98a0960aad2b07b746d4141b05443b6b357bc
DailyBalls <Stev@Stev-Development>
2025-10-11 12:59:48 +0700

    add deposit model

--- src/models/model_deposit.py
+++ src/models/model_deposit.py
@@ -6,6 +6,7 @@ class DepositChannel(BaseStateModel):
     name: Optional[str] = None
     type: Optional[str] = None
     code: Optional[str] = None
+    description: Optional[str] = None
     
 class AvailableDepositChannel(BaseStateModel):
     channels: Optional[list[DepositChannel]] = None
