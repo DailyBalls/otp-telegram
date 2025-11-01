1ceacef2c457c7b69840cd810d1dedd775dd1135
DailyBalls <Stev@Stev-Development>
2025-11-01 16:14:24 +0700

    Add reference field to model registration

--- src/models/model_register.py
+++ src/models/model_register.py
@@ -85,5 +85,6 @@ class ModelRegister(BaseStateModel):
             "nama": self.bank_account_name,
             "bank": self.bank_name,
             "captcha": self.fill_captcha,
+            "ref": None
         }
                 
